c443f7ac4b7f22b9d09e25f6c09b6d791ebb79cd
Иван Лаврущев <ivan_olympiad@mail.ru>
2025-04-08 03:54:54 +0300

    [ ADD ] New objects in gitignore and codestyle

--- inc/App.hpp
+++ inc/App.hpp
@@ -3,10 +3,10 @@
 
 static const double MOVING_SPEED  = 0.1;
 static const double SCALE_DEGREE  = 0.1;
-static const int    WINDOW_WIDTH  = 300;
+static const int    WINDOW_WIDTH  = 200;
 static const int    WINDOW_HEIGHT = 200;
-static const double REAL_HEIGHT = 2.0;
-static const double REAL_WIDTH  = 3.0;
+static const double REAL_HEIGHT   = 2.0;
+static const double REAL_WIDTH    = 3.0;
 static       int    WINDOW_SIZE   = WINDOW_WIDTH * WINDOW_HEIGHT;
 
 struct Coordinates {
@@ -16,7 +16,6 @@ struct Coordinates {
 
 struct ViewParameters {
     Coordinates top_left_angle  = {.x = -2, .y = 1};
-    // Coordinates bot_right_angle = {.x = (double)WINDOW_WIDTH, .y = (double)WINDOW_HEIGHT};
     double      x_shift = 0;
     double      y_shift = 0;
     double      scale   = 1;
